59563e55946fcfa181ee1e3da43b2806f8fc965d
Anil <anilcheguri97@gmail.com>
2024-12-07 06:54:22 +0530

    eureka server

---
+++ src/test/java/com/example/EurekaServerSample/EurekaServerSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EurekaServerSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
